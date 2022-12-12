b1e614c79dfab1ca17af10dd723acf61b2972089
Leon Patmore <leon.patmore@bath.edu>
2022-12-12 22:36:15 +0000

    handle gcp.

--- python_serverless/gcp/gcp_handler.py
+++ python_serverless/gcp/gcp_handler.py
@@ -1,14 +1,16 @@
+import os
+
 from ..serverless_http import ProviderHttpHandler, HttpRequest, HttpResponse
 
 
 class GcpHandler(ProviderHttpHandler):
 
     def generate_request(self, *args, **kwargs) -> HttpRequest:
-        event = kwargs["request"]
-        return HttpRequest(event.data)
+        event = args[0]
+        return HttpRequest(event.data.decode("utf-8"))
 
     def convert_to_response(self, response: HttpResponse):
         return response.body, response.status, {}
 
     def supports(self, *args, **kwargs) -> bool:
-        return "request" in kwargs
+        return "K_SERVICE" in os.environ.keys()
